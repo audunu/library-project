0673a2a94bead7fd331ee10ce8e51a63fa313ead
audunu <audunu@gmail.com>
2022-03-02 11:06:05 +0100

    work on basic layoyt and start work on functionality

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 let myLibrary = [];
 
+const bookListElement = document.querySelector('.book-list');
+const addBtn = document.querySelector('button[type=submit]');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('author');
+const pagesInput = document.querySelector('pages');
+const readInput = document.querySelector('read');
+const popUp = document.querySelector('.pop-up');
+
 class Book {
     constructor(title, author, pages, read) {
         this.title = title;
@@ -12,9 +20,31 @@ class Book {
     }
 }
 
+addBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    popUp.style.visibility = 'hidden';
+    addBookToLibrary();
+})
+
+
+
+
+function addBookToLibrary() {
+    
+    
+    const newBook = document.createElement('li');
+    newBook.innerHTML = theHobbit.info();
+    bookListElement.appendChild(newBook);
+}
+
+
 
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read');
 const exampleBook = new Book('Title', 'Person', 290, 'read');
+
+myLibrary.push(theHobbit);
+myLibrary.push(exampleBook);
+
 
 console.log(theHobbit.info());
